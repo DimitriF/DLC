60dcdfc4a849f893c4e801d5c5b35764a83c9af1
dimitrif <dimitrifichou@gmail.com>
2018-12-17 17:24:15 +0200

    redim.array with width.out

--- R/raster.R
+++ R/raster.R
@@ -2,7 +2,7 @@
 ##' @param data an array, or a matrix
 ##' @param main an optionnal title
 ##' @param Title.dim should the dimension be indicated on the plot
-##' @param respect will adjust the number of pixel depending of the image dimension and plotting region.
+##' @param inv set pixel 0,0 in top left as it should be, usefull for interactive plot.
 ##' @param ... Arguments to be passed to methods
 ##' @examples
 ##' data <- f.read.image(source='www/rTLC_demopicture.JPG',height=128)
@@ -10,26 +10,22 @@
 ##' @author Dimitri Fichou
 ##' @export
 
-raster <- function(data,main='',Title.dim=F,interpolate = F,respect=F,...){ # a 3D array
+raster <- function(data,main='',Title.dim=F,interpolate = F,inv = F,...){ # a 3D array
   # par(mar=c(0,0,2,0),oma=c(0,0,2,0))
   if(length(dim(data))>2){if(dim(data)[3] > 3){data <- data[,,c(1,2,3)]}}
   # print('SOM.cluster.plot.picture')
   # print(dim(data))
   if(Title.dim == T){main=paste0('Dimensions: ',paste(dim(data),collapse='-'),'\n',main)}
-  if(!respect){
-    plot(c(0,dim(data)[2]),c(0,dim(data)[1]), type='n',ylab="",xlab="",main=main,...)
-  }else{
-    par(xaxs="i",yaxs="i")
-    ## check which is the limited dimension
-    if(par("pin")[1]/par("pin")[2] <= dim(data)[2]/dim(data)[1]){
-      plot(c(0,dim(data)[2]),c(0,dim(data)[2]/par("pin")[1]*par("pin")[2]), type='n',ylab="",xlab="",main=main,...)
-    }else{
-      plot(c(0,dim(data)[1]/par("pin")[2]*par("pin")[1]),c(0,dim(data)[1]), type='n',ylab="",xlab="",main=main,...)
-    }
 
-  }
+  plot(c(0,dim(data)[2]),if(inv){c(dim(data)[1],0)}else{c(0,dim(data)[1])}, type='n',ylab="",xlab="",main=main,
+       ylim = if(inv){c(dim(data)[1],0)}else{c(0,dim(data)[1])},...)
 
-  rasterImage(data,0,0,dim(data)[2],dim(data)[1],interpolate=interpolate)
+  rasterImage(data,
+              xleft = 0,
+              ybottom = if(inv){dim(data)[1]}else{0},
+              xright = dim(data)[2],
+              ytop = if(!inv){dim(data)[1]}else{0},
+              interpolate=interpolate)
 
 }
 # raster(rtlc,respect=T)
--- R/redim.array.R
+++ R/redim.array.R
@@ -2,6 +2,7 @@
 ##'
 ##' @param data 3D array
 ##' @param height.out number of pixel in height (first dimension of the 3D array)
+##' @param width.out number of pixel in width, optionnal, if not specified will be infered from the height and array dimension (second dimension of the 3D array)
 ##' @examples
 ##' data <- f.read.image('www/rTLC_demopicture.JPG',format='jpeg',native=F)
 ##' data <- data %>% redim.array(256)
@@ -9,14 +10,20 @@
 ##' @author Dimitri Fichou
 ##' @export
 
-redim.array <- function(data,height.out){
+redim.array = function (data, height.out, width.out = NULL)
+{
   height.in = dim(data)[1]
   width.in = dim(data)[2]
-  width.out = ceiling(height.out/height.in*width.in)
-  if(length(dim(data)) == 3){
-    data <- data[floor(seq(1,height.in,length.out=height.out)),floor(seq(1,width.in,length.out=width.out)),]
-  }else{
-    data <- data[floor(seq(1,height.in,length.out=height.out)),floor(seq(1,width.in,length.out=width.out))]
+  if(is.null(width.out)){
+    width.out = ceiling(height.out/height.in * width.in)
+  }
+  if (length(dim(data)) == 3) {
+    data <- data[floor(seq(1, height.in, length.out = height.out)), , ]
+    data <- data[,floor(seq(1, width.in, length.out = width.out)),]
+  }
+  else {
+    data <- data[floor(seq(1, height.in, length.out = height.out)),
+                 floor(seq(1, width.in, length.out = width.out))]
   }
   return(data)
 }
